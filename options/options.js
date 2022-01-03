2df8a3f8a542c3680c8363067a1595817cc1917c
Crowdbotics <team@crowdbotics.com>
2022-01-03 16:39:03 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,14 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "ideapros_llc_vista_32437",
   url: "https://ideapros_llc_vista_32437.botics.co",
   api: "https://ideapros_llc_vista_32437.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
+    copy: "Routes available!"
+  },
+  undefined: { title: "App Menu", copy: "Routes available!", styles: styles }
 }
